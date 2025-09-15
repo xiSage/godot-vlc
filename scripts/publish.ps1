72b4339015e9180e326b2574d736342ce2e4c8c8
xiSage <57551864+xiSage@users.noreply.github.com>
2025-09-15 23:52:43 +0800

    move lib to platform specific directory

--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -1,4 +1,7 @@
 . scripts/build_debug.ps1
 . scripts/build_release.ps1
 $manifest = cargo read-manifest | ConvertFrom-Json
-Compress-Archive -Path "demo/addons" -DestinationPath "$($manifest.name)_v$($manifest.version).zip" -Force
+$fileName = "$($manifest.name)_v$($manifest.version).zip"
+# Compress-Archive -Path "demo/addons" -DestinationPath "$($manifest.name)_v$($manifest.version).zip" -Force
+Remove-Item -Path $fileName -ErrorAction SilentlyContinue
+[System.IO.Compression.ZipFile]::CreateFromDirectory("demo/addons", $fileName, [System.IO.Compression.CompressionLevel]::Optimal, $true)
